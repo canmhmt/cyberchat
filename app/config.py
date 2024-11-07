4dab5f42da4e55ccd883f0f5f79176ba8e0543a8
TheClassy08 <canmhmtgt@gmail.com>
2024-11-07 16:32:00 +0300

    update

--- app/config.py
+++ app/config.py
@@ -10,10 +10,3 @@ def db_connection():
     )
 
     return connection
-
-MAIL_SERVER = 'smtp.mailgun.org'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'postmaster@your_domain.mailgun.org'
-MAIL_PASSWORD = 'your_mailgun_password'
-MAIL_DEFAULT_SENDER = 'your_email@your_domain.com'
